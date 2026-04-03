88faeb0777b5d917c0e8b6cfef920ffd8f92d5aa
emecuce <manuquirno@hotmail.com>
2026-04-03 15:11:57 -0300

    Add PythonAnywhere ASGI entrypoint

---
+++ asgi.py
@@ -0,0 +1,9 @@
+from main import app
+import os
+import sys
+
+project_home = "/home/cprargentina/cPRArgentina"
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ["CPRA_DB"] = "cpra.db"
